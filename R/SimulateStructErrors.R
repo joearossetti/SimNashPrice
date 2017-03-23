1f89a73bd3c3c96bd97299a098de1439a8850016
Joseph Rossetti <joe.a.rossetti@gmail.com>
2017-03-23 10:35:49 -0400

    Change how this function works, use closures to draw from error distributions rather than functions that return a list of draws given arguements .... Added mc errors to function.

--- R/SimulateStructErrors.R
+++ R/SimulateStructErrors.R
@@ -1,59 +1,28 @@
-#' Title Simulate Markets with different Structural Errors
+#' Simulate Markets with different Structural Errors
 #'
 #'  This function takes a 'Logit_Demand_Market' object
 #'  and a function that generates draws from th structural errors of the model (either estimates from GMM or parametric assumption)
 #'  Calculates nash prices and returns a list of 'Logit_Demand_Market's based on each draw from the errors
 #'
 #' @param logit_market 'Logit_Demand_Market' object
-#' @param struct_error_draw_fun function that returns a list of market objects length draws evaluated at each draw from the errors
+#' @param struct_error_draw_fun function that takes the number of products as its only arguement and returns a sample fromr the dist. of structural errors (use a closure)
 #' @param draws numer of draws to take (passed to struct_error_draw_fun)
 #' @param tol tolerance for the fixed point
+#' @param mc_error_draw_fun function that takes the number of products as its only arguement and returns a sample fromr the dist. of mc errors (use a closure)
 #' @param logit_market_method a method for the class 'Logit_Demand_Market' to apply to each draw (for example computing firm profits)
-#' @param ... additional arguments to struct_error_draw_fun
 #'
 #' @return list of 'Logit_Demand_Market' object with the NE corresponding to the draws from the structural errors
 #' @export
 #'
 #' @examples
-#' set.seed(1234)
-#'
-#'## Parameters of demand function
-#'alpha <- 4
-#'beta <- c(2, 3.5)
-#'gamma <- c(0.5, 0.3)
-#'
-#'## Make a list of factor level
-#'char_levels <- list(char1 = c(0,1), char2 = c(0,1))
-#'
-#'## Make list of all potential products
-#'XP <- expand.grid(char_levels)
-#'J <- dim(XP)[1]
-#'
-#'XP <- data.frame(XP, prodname=1:J)
-#'
-#'a1 <- sample(c(0,1), 4, replace = TRUE)
-#'a2 <- sample(c(0,1), 4, replace = TRUE)
-#'
-#'Xi <- rnorm(1, 0, 1)
-#'Omega <- rnorm(1, 0, 1)
-#'
-#'my_prod_table <- data.frame(j = XP$prodname, delta = (cbind(XP$char1, XP$char2) %*% beta), mc = (cbind(XP$char1, XP$char2) %*% gamma))
-#'
-#'my_mkt_prods <- rbind(my_prod_table[which(a1==1),], my_prod_table[which(a2==1),])
-#'my_mkt_prods <- data.frame(my_mkt_prods, firm = c(1,1,1,0,0))
-#'
-#'my_ldm_obj <- logit_demand_market(my_mkt_prods$firm, my_mkt_prods$delta, Mc = my_mkt_prods$mc, Struct_error = Xi, Deriv_price = alpha)
-#'
-#'my_struct_error_fun <- function(draws, ...){
-#' rnorm(n=draws, ...)
-#'}
-#'
-#'my_markets <- sim_struct_errors(logit_market=my_ldm_obj, struct_error_draw_fun=my_struct_error_fun, logit_market_method=firm_profits,draws=500, tol=1e-6, mean=0, sd=1)
-sim_struct_errors <- function(logit_market, struct_error_draw_fun, logit_market_method, draws, tol, ...){
-  errors <- as.list(struct_error_draw_fun(draws, ...))
+#' #NA
+sim_struct_errors <- function(logit_market, struct_error_draw_fun, mc_error_draw_fun, logit_market_method, draws, tol){
   markets <- vector('list', draws)
   for(i in 1:draws){
-    logit_market$Market$Struct_Err <- errors[[i]]
+    number_of_products <- length(logit_market$Market)
+    logit_market$Market$Struct_Err <- struct_error_draw_fun(number_of_products)
+    logit_market$Market$Mc_error <- mc_draw_fun(number_of_products)
+    logit_market <- Mc_method(logit_market)
     markets[[i]] <- zeta_fixed_point(logit_market, tol = tol)
     markets[[i]] <- logit_market_method(markets[[i]])
   }
