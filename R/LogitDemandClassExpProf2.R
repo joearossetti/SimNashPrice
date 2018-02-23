5da044bc9d2b1d2ca735e0d2b71f7aa5c60ecaf8
Joseph Rossetti <joe.a.rossetti@gmail.com>
2018-02-23 14:39:02 -0500

    Compute expected variable profit using pre-drawn matrix of disturbances

---
+++ R/LogitDemandClassExpProf2.R
@@ -0,0 +1,63 @@
+#' Expected Variable Profits
+#'
+#'  Given a pair of functions for drawing from the distribution of the econometric errors,
+#'  draw from the distribution, run the Zeta Fixed Point to re-compute prices and profits for each draw,
+#'  and find the expected variable profits.
+#'
+#' @param mc_error_fun function for drawing from the marginal cost errors
+#' @param struct_error_fun function for drawing from the utility errors
+#' @param draws the number of draws to take
+#' @param tol when should the zeta fixed point converge
+#'
+#' @return vector of each firms expected profits
+#' @export
+#'
+#' @examples #NA
+ldmkt_exp_profits_mat <- function(mc_error_mat, struct_error_mat, draws, tol, Max_iter, compute_inc_value=FALSE){
+  assertthat::assert_that(all(dim(mc_error_mat)==dim(struct_error_mat)))
+  assertthat::assert_that(dim(mc_error_mat)[1]==draws)
+  assertthat::assert_that(dim(mc_error_mat)[2]==private$Jt)
+
+  var_prof_mat <- matrix(nrow = draws, ncol = private$num_firms)
+  if(compute_inc_value==TRUE){
+    inc_val_vec <- vector('numeric', length = draws)
+  }
+  for(i in 1:draws){
+    private$ujs <- private$Market[['Delta']] + t(as.numeric(struct_error_mat[i,]))
+    # print(length(private$Market[['Delta']]))
+    # print(length(struct_error_mat[i,]))
+    # print(length(as.numeric(struct_error_mat[i,])))
+    # print(dim(t(as.numeric(struct_error_mat[i,]))))
+    # print(dim(private$ujs))
+    # print(dim(private$uijs))
+    # print(dim(private$Market[["Price"]]))
+    # print(dim(private$Deriv_price))
+
+    private$cjs <- exp(private$Market[['Mc_fixed']])*exp(t(as.numeric(mc_error_mat[i,])))
+
+
+    self$zeta_fixed_point(tol=tol, max_iter=Max_iter)
+
+    for(j in 1:private$num_firms){
+      f <- private$firm_names[j]
+      Mkt <- private$Market
+      which_prods <- which(Mkt[['Firms']]==f)
+      var_prof_mat[i,j] <- sum((as.numeric(Mkt[['Price']])[which_prods]-private$cjs[which_prods])*as.numeric(Mkt[['Share']])[which_prods])
+    }
+
+    if(compute_inc_value==TRUE){
+      Index <- private$uijs - private$Market[['Price']] %*% private$Deriv_price
+      S <- exp(Index)
+      Denom <- exp(private$U_out_opt) + colSums(S)
+      inc_val_vec[i] <- mean(log(Denom) / private$Deriv_price)
+    }
+
+    private$cjs <- 0
+  }
+
+  profits <- colSums(var_prof_mat)/draws
+  if(compute_inc_value==TRUE){
+    attr(profits, "inc_value") <- sum(inc_val_vec)/draws
+  }
+  return(profits)
+}
