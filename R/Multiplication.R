c18aba436819626f097b7964bd97cb6b542e430a
Joseph Rossetti <joe.a.rossetti@gmail.com>
2018-03-07 15:39:21 -0500

    redefine cube/field operations vec*cube returns a mat vec*field returns a cube mat*cube returns a cube Operations properly stack vertically (as if seperate equations are stacked)

--- R/Multiplication.R
+++ R/Multiplication.R
@@ -1,7 +1,7 @@
 vector_times_cube <- function(vec, cube, com_dim){
   mat <- matrix(0, com_dim, com_dim)
   for(j in 1:com_dim){
-    mat <- mat + vec[j] * cube[,,j]
+    mat[j,] <- cube[,,j] %*% vec
   }
   return(mat)
 }
@@ -9,17 +9,20 @@ vector_times_cube <- function(vec, cube, com_dim){
 vector_times_cube2 <- function(vec, cube, com_dim, dim){
   mat <- matrix(0, dim[1], dim[2])
   for(j in 1:com_dim){
-    mat <- mat + vec[j] * cube[,,j]
+    mat[j,] <- cube[,,j] %*% vec
   }
   return(mat)
 }
 
 vector_times_field <- function(vec, field, com_dim){
   cube <- array(0, dim=rep(com_dim, 3))
-  for(j in 1:com_dim){
-    for(k in 1:com_dim){
-      cube[,,j] <- cube[,,j] + vec[k] * field[[k]][,,j]
-    }
+  # for(j in 1:com_dim){
+  #   for(k in 1:com_dim){
+  #     cube[,,j] <- cube[,,j] + vec[k] * field[[k]][,,j]
+  #   }
+  # }
+  for(k in 1:com_dim){
+    cube[,,j] <- vector_times_cube(vec = vec, cube = field[[k]], com_dim = com_dim)
   }
   return(cube)
 }
